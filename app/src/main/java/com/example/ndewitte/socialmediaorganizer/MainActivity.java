637529caae5107a4681d940f941f6943881d4ac8
nolandewitte <16ndewitte@KENB083FEB4D355.crcsd.abc>
2016-02-09 08:27:34 -0600

    Initial Commit

---
+++ app/src/main/java/com/example/ndewitte/socialmediaorganizer/MainActivity.java
@@ -0,0 +1,79 @@
+package com.example.ndewitte.socialmediaorganizer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView ytWebView = (WebView) findViewById(R.id.youtubeWeb);
+        WebSettings webSettings = ytWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        ytWebView.setWebViewClient(new WebViewClient() {
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+        ytWebView.loadUrl("http://www.youtube.com");
+
+        WebView tWebView = (WebView) findViewById(R.id.twitterWeb);
+        tWebView.setWebViewClient(new WebViewClient() {
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }});
+        tWebView.loadUrl("http://www.twitter.com");
+
+        WebView rWebView = (WebView) findViewById(R.id.redditWeb);
+        rWebView.setWebViewClient(new WebViewClient() {
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }});
+        rWebView.loadUrl("http://www.reddit.com");
+
+        WebView ifWebView = (WebView) findViewById(R.id.iFunnyWeb);
+        ifWebView.setWebViewClient(new WebViewClient() {
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }});
+        ifWebView.loadUrl("http://www.ifunny.com");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }}
+
+
+
+
